e647fe6ef023a6dc03fd1612248b85fbd2219538
xumianjun <xumianjun@jd.com>
2018-12-07 20:28:55 +0800

    Initial Commit

---
+++ ZZDesignPatterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZDesignPatterns
+//
+//  Created by 徐勉俊 on 2018/12/7.
+//  Copyright © 2018 Jungle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZDesignPatterns/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZZDesignPatterns
+//
+//  Created by 徐勉俊 on 2018/12/7.
+//  Copyright © 2018 Jungle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
